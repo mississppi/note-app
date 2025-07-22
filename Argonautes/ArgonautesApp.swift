39605ac88c2aac335dbb29ad259440706f87bd28
mississppi <msspstudio@gmail.com>
2025-07-22 22:17:31 +0900

    initial commit

---
+++ Argonautes/ArgonautesApp.swift
@@ -0,0 +1,20 @@
+//
+//  ArgonautesApp.swift
+//  Argonautes
+//
+//  Created by KOSUKE SAKURAI on 2025/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ArgonautesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
