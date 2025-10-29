8aca6b89cf911420ca25f1aac3f1e79eaa1df61e
mississppi <msspstudio@gmail.com>
2025-10-29 23:37:01 +0900

    fix title

--- Argonautes/Views/Detail/NoteDetailTitleView.swift
+++ Argonautes/Views/Detail/NoteDetailTitleView.swift
@@ -4,7 +4,7 @@ struct NoteDetailTitleView: View {
     @ObservedObject var viewModel: NoteListViewModel
     
     var body: some View {
-        TextField("タイトル", text: $viewModel.selectedTitle)
+        TextField("", text: $viewModel.selectedTitle)
             .font(.largeTitle)
             .fontWeight(.bold)
             .textFieldStyle(PlainTextFieldStyle()) // 枠線を外す
