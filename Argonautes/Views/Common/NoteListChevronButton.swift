862e946fb1c594b0367a65604e3bb78041ae8a00
mississppi <msspstudio@gmail.com>
2025-11-09 23:38:23 +0900

    mod bu

--- Argonautes/Views/Common/NoteListChevronButton.swift
+++ Argonautes/Views/Common/NoteListChevronButton.swift
@@ -11,12 +11,30 @@ struct NoteListChevronButton: View {
     
     var body: some View {
         Button(action: action) {
-            Image(systemName: direction == .left ? "chevron.left" : "chevron.right")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 24, height: 24)
-                .foregroundColor(.black)
+            // ZStack {
+            //     Color.clear
+            //     .frame(width: 44, height: 44)
+            //     .contentShape(Rectangle())
+
+            //     Image(systemName: direction == .left ? "chevron.left" : "chevron.right")
+            //         .resizable()
+            //         .scaledToFit()
+            //         .frame(width: 24, height: 24)
+            //         .foregroundColor(.black)
+            // }
+            HStack {
+                if direction == .right { Spacer(minLength: 0) }
+                Image(systemName: direction == .left ? "chevron.left" : "chevron.right")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 18, height: 18)
+                    .padding(direction == .left ? .leading : .trailing, 2)
+                if direction == .left { Spacer(minLength: 0 )}
+            }
+            .frame(width: 44, height: 44)
+            .contentShape(Rectangle())
         }
         .buttonStyle(PlainButtonStyle())
+        .accessibilityLabel(direction == .left ? "前のタグ" : "次のタグ")
     }
 }
