cd38517abca00344aeb0aa298530124929e80589
mississppi <msspstudio@gmail.com>
2025-11-09 23:43:08 +0900

    mod code

--- Argonautes/Views/Common/NoteListChevronButton.swift
+++ Argonautes/Views/Common/NoteListChevronButton.swift
@@ -11,17 +11,6 @@ struct NoteListChevronButton: View {
     
     var body: some View {
         Button(action: action) {
-            // ZStack {
-            //     Color.clear
-            //     .frame(width: 44, height: 44)
-            //     .contentShape(Rectangle())
-
-            //     Image(systemName: direction == .left ? "chevron.left" : "chevron.right")
-            //         .resizable()
-            //         .scaledToFit()
-            //         .frame(width: 24, height: 24)
-            //         .foregroundColor(.black)
-            // }
             HStack {
                 if direction == .right { Spacer(minLength: 0) }
                 Image(systemName: direction == .left ? "chevron.left" : "chevron.right")
