172814cf749bd4f89b83afa68002cc6da8deaeaf
mississppi <msspstudio@gmail.com>
2025-12-01 23:15:49 +0900

    change notelist

--- Argonautes/Views/NoteList/List/NoteListNoteArea.swift
+++ Argonautes/Views/NoteList/List/NoteListNoteArea.swift
@@ -5,50 +5,44 @@ import UniformTypeIdentifiers
 struct NoteListNoteArea: View {
     @ObservedObject var viewModel: NoteListViewModel
     
-    private var selectionBinding: Binding<Note?> {
-        Binding<Note?>(
-            get: { viewModel.selectedNote },
-            set: { new in
-                Task {
-                    await MainActor.run {
-                        // ゴミ箱表示中なら閉じる
-                        viewModel.selectNote(
-                            new,
-                            closeTrashIfNeeded: true,
-                            userInitiated: false
-                        )
-                    }
-                }
-            }
-        )
-    }
+    // private var selectionBinding: Binding<Note?> {
+    //     Binding<Note?>(
+    //         get: { viewModel.selectedNote },
+    //         set: { new in
+    //             Task {
+    //                 await MainActor.run {
+    //                     // ゴミ箱表示中なら閉じる
+    //                     viewModel.selectNote(
+    //                         new,
+    //                         closeTrashIfNeeded: true,
+    //                         userInitiated: false
+    //                     )
+    //                 }
+    //             }
+    //         }
+    //     )
+    // }
     
     var body: some View {
-        // List(selection: selectionBinding) {
-        //     ForEach(viewModel.notes, id: \.self) { note in
-        //         NoteRowView(note: note)
-        //             .contextMenu {
-        //                 NoteListArchiveButton(viewModel: viewModel, note: note)
-        //             }
-        //     }
-        //     .onMove { fromOffsets, toOffset in
-        //         viewModel.moveNotes(fromOffsets: fromOffsets, toOffset: toOffset)
-        //     }
-        // }
-        List(selection: selectionBinding) {
+        List{
             ForEach(viewModel.notes, id: \.self) {
                 note in
                 NoteRowView(note: note)
                     .contentShape(Rectangle())
-                    // .onTapGesture {
-                    //     selectionBinding.wrappedValue = note
-                    // }
+                    .onTapGesture {
+                        viewModel.selectNote(
+                            note,
+                            closeTrashIfNeeded: true,
+                            userInitiated: false
+                        )
+                    }
                     .contextMenu {
                         NoteListArchiveButton(viewModel: viewModel, note: note)
                     }
                     .listRowBackground(
                         (viewModel.selectedNote?.objectID == note.objectID) ? Color(hex: "#E6E6E6") : Color.clear
                     )
+                    // .listRowSeparatorTint(.clear)
             }
             .onMove { fromOffsets, toOffset in
                 viewModel.moveNotes(fromOffsets: fromOffsets, toOffset: toOffset)
@@ -58,6 +52,7 @@ struct NoteListNoteArea: View {
         .listStyle(.plain)
         .scrollContentBackground(.hidden)
         .background(Color(hex: "EFEFEF"))
+        // .tint(.clear)
         .onAppear{
             viewModel.fetchNotes()
         }
